4b9310c56be35329ba6005e62e1d82bcbf973ab6
Dillon Nys <dnys1@asu.edu>
2021-01-23 16:42:41 -0700

    Clean up static build tool

--- web/static/tool/grind.dart
+++ web/static/tool/grind.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:glob/glob.dart';
 import 'package:grinder/grinder.dart';
 
 main(args) => grind(args);
@@ -13,42 +12,12 @@ test() => TestRunner().testAsync();
 // @Depends()
 build() {
   // Build index.html
-  PubApp.local('build_runner').run(['build', '-r', '-o', 'build']);
-
-  // Build main.dart
-  Dart2js.compile(
-    File('lib/main.dart'),
-    outDir: joinDir(Directory.current, ['js']),
-    minify: true,
-  );
-
-  // Copy over static assets
-  final staticAssets = {
-    'web/css/*.css': 'css',
-    'web/js/*.js': 'js',
-    'web/img': 'img',
-    'web/*.html': '',
-  };
-
-  final buildDir = joinDir(Directory.current, ['build']);
-  for (final entry in staticAssets.entries) {
-    final glob = Glob(entry.key);
-    final dir = joinDir(buildDir, [entry.value]);
-    for (final fileEnt in glob.listSync()) {
-      copy(fileEnt, dir);
-    }
-  }
+  PubApp.local('build_runner').run(['build', '-r', '-o', 'web:build']);
 }
 
 @Task()
+@Depends(build)
 serve() async {
-  // Build main.dart
-  Dart2js.compile(
-    File('lib/main.dart'),
-    outFile: File('web/js/main.js'),
-    minify: false,
-  );
-
   await Process.start(Platform.executable, ['bin/serve.dart'])
       .then((Process process) {
     process.stdout.transform(utf8.decoder).listen(stdout.write);
